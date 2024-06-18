7bcefaf3c81ed74a88b485673ad40247631d144d
Lily W <lilywu256@gmail.com>
2024-06-19 08:32:44 +0900

    View lesson table
    Lesson table with most basic formatting and functionality

--- js/lessontable.js
+++ js/lessontable.js
@@ -3,7 +3,7 @@ function getLessons() {
     // https://www.js-tutorials.com/jquery-tutorials/reading-csv-file-using-jquery/
     // still trying to wrap my head around this...
     let lessonsList;
-    let lessonsDict;
+    let lessonsRead = [];
 
     $.ajax({
         type: "GET",  
@@ -11,14 +11,38 @@ function getLessons() {
         dataType: "text",       
         success: function(response)  {
             lessonsList = $.csv.toArrays(response);
-          
-            for (let i = 1; i < lessonsList.length; i++) {
-                // add lesson to dictionary
-            }   
-        }   
-      });
 
-      console.log(lessonsList);
+            for (let i = 1; i < lessonsList.length; i++) {
+                let lesson = {
+                    year: lessonsList[i][0],
+                    unit: lessonsList[i][1],
+                    grammar: lessonsList[i][2],
+                    type: lessonsList[i][3],
+                    duration: lessonsList[i][4],
+                    activity: lessonsList[i][5],
+                    link: lessonsList[i][6],
+                };
+                lessonsRead.push(lesson);
+            }   
+
+            let table = new Tabulator("#lessontable", {
+                data: lessonsRead, // assign data to table
+                layout: "fitDataStretch", // fit columns to width of table (optional)
+                columns: [ //Define Table Columns
+                    {title: "Grammar", field: "grammar"},
+                    {title: "Year", field: "year"},
+                    {title: "Unit", field: "unit"},
+                    {title: "Duration", field: "duration"},
+                    {title: "Lesson Description", field: "activity", formatter: "textarea"}
+                ],
+                });
+           
+            //trigger an alert message when the row is clicked
+            table.on("rowClick", function(e, row){ 
+                window.open(row.getData().link, '_blank').focus();
+            });
+        }
+    });
 }
 
-getLessons();
+console.log(getLessons());
